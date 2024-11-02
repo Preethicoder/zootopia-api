42a24e384ce52aad14a9478ac4fcd9013f77bd0f
Preethicoder <preethisivakumar2024@gmail.com>
2024-11-02 18:51:20 +0100

    added new method serialize_animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,24 +7,30 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-output = ''
-for animal in animals_data:
+def serialize_animal(animal):
+    output = ''
     output += '<li class="cards__item">\n'
-    output +='<div class="card__title">'+ f'Name: {animal["name"]}'+'</div>\n'
-    output +='<p class="card__text">\n'
-    output += '<strong>Diet: </strong>'+f"{animal['characteristics']['diet']}<br/>\n"
-    output += '<strong>Location: </strong>'+f"{animal['locations'][0]}<br/>\n"
+    output += '<div class="card__title">' + f'Name: {animal["name"]}' + '</div>\n'
+    output += '<p class="card__text">\n'
+    output += '<strong>Diet: </strong>' + f"{animal['characteristics']['diet']}<br/>\n"
+    output += '<strong>Location: </strong>' + f"{animal['locations'][0]}<br/>\n"
     if "type" in animal["characteristics"]:
-        output +='<strong>Type: </strong>'+f"{animal['characteristics']['type']}<br/>\n"
+        output += '<strong>Type: </strong>' + f"{animal['characteristics']['type']}<br/>\n"
     output += " </p>\n"
     output += '</li>'
+    return output
+
+result =""
+for animal in animals_data:
+    result += serialize_animal(animal)
+
 
 
 
 with open("animals_template.html","r") as handle:
     html_content = handle.read()
 
-html_content = html_content.replace("__REPLACE_ANIMALS_INFO__",output)
+html_content = html_content.replace("__REPLACE_ANIMALS_INFO__",result)
 
 with open("animals_template.html","w") as handle1:
     handle1.write(html_content)
